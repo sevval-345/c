6d187308a01e808f01a2635b3437e05e11c63ac9
sevval-345 <116575478+sevval-345@users.noreply.github.com>
2023-09-06 16:34:17 +0300

    Add files via upload

---
+++ 234/ex06/ft_print_comb2.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smikci <smikci@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 19:10:52 by smikci            #+#    #+#             */
+/*   Updated: 2023/09/06 15:30:45 by smikci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar((a / 10) + 48);
+			ft_putchar((a % 10) + 48);
+			ft_putchar(' ');
+			ft_putchar((b / 10) + 48);
+			ft_putchar((b % 10) + 48);
+			if (a != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' '); 
+			}
+			b++;
+		}
+		a++;
+	}
+}
